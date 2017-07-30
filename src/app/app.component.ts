080094c5412624e00e803b0292413ccfffe324ee
Jacobo <jacob.et.cetera@gmail.com>
2017-07-30 17:51:07 -0500

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    nombre:string = 'Jacobo';
+
+    arreglo:number[] = [1,2,3,4,5,6,7,9,10];
+
+    PI:number = Math.PI;
+
+    percentage:number = 0.234;
+
+    money:number = 13213.25;
+
+    heroe:any = {
+        nombre: "Logan",
+        clave: "Wolverine",
+        casa: {
+            calle: "Ignacio",
+            numero: 5
+        }
+    }
+
+    valorDePromesa = new Promise( (resolve,reject)=>{
+
+        setTimeout( ()=>resolve('Llego la data'), 3500 );
+
+    } );
+
+    video:string = '6wJ1lMgtHFE';
 }
